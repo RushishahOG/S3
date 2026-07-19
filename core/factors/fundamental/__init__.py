359501c2f24a917bf4501e7aa0ac9a4e422f04a3
RushishahOG <rushishah004@gmail.com>
2026-07-19 09:03:16 +0530

    Export individual compute functions from core.factors.fundamental

--- core/factors/fundamental/__init__.py
+++ core/factors/fundamental/__init__.py
@@ -3,11 +3,35 @@
 Exports:
     FundamentalQualityEngine: Engine to compute 16 Quality factors from screener data.
     QUALITY_FACTOR_FUNCTIONS: Dictionary of available quality factor compute functions.
+    compute_*: Individual factor computation functions.
 """
 
 from .engine import (
     FundamentalQualityEngine,
     QUALITY_FACTOR_FUNCTIONS,
+    compute_cash_roce,
+    compute_dividend_payout_ratio,
+    compute_dividend_payout_ratio_cumulative,
+    compute_equity_to_total_capital,
+    compute_interest_coverage_ratio,
+    compute_ocf_to_ebitda,
+    compute_roa,
+    compute_roce,
+    compute_roe,
+    compute_sustainable_growth_rate,
 )
 
-__all__ = ["FundamentalQualityEngine", "QUALITY_FACTOR_FUNCTIONS"]
+__all__ = [
+    "FundamentalQualityEngine",
+    "QUALITY_FACTOR_FUNCTIONS",
+    "compute_cash_roce",
+    "compute_dividend_payout_ratio",
+    "compute_dividend_payout_ratio_cumulative",
+    "compute_equity_to_total_capital",
+    "compute_interest_coverage_ratio",
+    "compute_ocf_to_ebitda",
+    "compute_roa",
+    "compute_roce",
+    "compute_roe",
+    "compute_sustainable_growth_rate",
+]
